61adc44c72eb14b83f68f5a7a9c8af2c3d9f318f
MANNE UDAY KIRAN <udaykiran7615124@gmail.com>
2026-06-20 17:41:11 +0530

    made changes in ui

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["localhost", "127.0.0.1", "https://ecosyn-ai-powered-carbon-footprint.onrender.com/","https://frontend-six-nu-62.vercel.app"]
+  }
 })
